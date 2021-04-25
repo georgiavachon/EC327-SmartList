368f772d74ac4e865f18cb5931991fde37526626
dsmolina <79925931+dsmolina@users.noreply.github.com>
2021-04-25 15:51:50 -0400

    Update EC327-SmartList.cpp

--- EC327-SmartList.cpp
+++ EC327-SmartList.cpp
@@ -1,5 +1,5 @@
-// Copyright Megan Freeman 2021 megfree@bu.edu
 // Copyright Dasha Smolina 2021 dsmolina@bu.edu
+// Copyright Megan Freeman 2021 megfree@bu.edu
 // Copyright Ayrton Reulet 2021 reulayrt@bu.edu
 // Copyright Georgia Vachon gvachon@bu.edu
 #include <string>
